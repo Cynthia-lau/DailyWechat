0343c14d2e5ec683c73c1936d002d0ef77515606
Cynthia-lau <MINGYUELIU520@GMAIL.COM>
2026-03-30 22:37:20 +0800

    Refactor get_words function and improve error handling

--- main.py
+++ main.py
@@ -8,20 +8,21 @@ import requests
 nowtime = datetime.utcnow() + timedelta(hours=8)
 today = datetime.strptime(str(nowtime.date()), "%Y-%m-%d")
 
-
 def get_time():
     dictDate = {'Monday': '星期一', 'Tuesday': '星期二', 'Wednesday': '星期三', 'Thursday': '星期四',
-                'Friday': '星期五', 'Saturday': '星期六', 'Sunday': '星期天'}
+    'Friday': '星期五', 'Saturday': '星期六', 'Sunday': '星期天'}
     a = dictDate[nowtime.strftime('%A')]
     return nowtime.strftime("%Y年%m月%d日") + a
 
-
 def get_words():
-    words = requests.get("https://tenapi.cn/v2/yiyan?format=json").json()
-    print(words)
-    if words['code'] != 200:
-        return get_words()
-    return words['data']['hitokoto']
+    try:
+        resp = requests.get("https://tenapi.cn/v2/yiyan?format=json", timeout=10)
+        words = resp.json()
+        if words.get('code') == 200:
+            return words['data']['hitokoto']
+    except Exception:
+        pass
+    return "每天都是新的开始。"
 
 def get_weather(city, key):
     url = f"https://api.seniverse.com/v3/weather/daily.json?key={key}&location={city}&language=zh-Hans&unit=c&start=-1&days=5"
@@ -35,13 +36,11 @@ def get_count(born_date):
     delta = today - datetime.strptime(born_date, "%Y-%m-%d")
     return delta.days
 
-
 def get_birthday(birthday):
     nextdate = datetime.strptime(str(today.year) + "-" + birthday, "%Y-%m-%d")
     if nextdate < today:
         nextdate = nextdate.replace(year=nextdate.year + 1)
     return (nextdate - today).days
-
 
 if __name__ == '__main__':
     app_id = os.getenv("APP_ID")
@@ -55,22 +54,21 @@ if __name__ == '__main__':
     f = open("users_info.json", encoding="utf-8")
     js_text = json.load(f)
     f.close()
-    data = js_text['data']
+    users = js_text['data']
     num = 0
-    words=get_words()
-    out_time=get_time()
+    words = get_words()
+    out_time = get_time()
 
     print(words, out_time)
 
-    for user_info in data:
+    for user_info in users:
         born_date = user_info['born_date']
         birthday = born_date[5:]
         city = user_info['city']
         user_id = user_info['user_id']
         name = user_info['user_name'].upper()
 
-
-        wea_city,weather = get_weather(city,weather_key)
+        wea_city, weather = get_weather(city, weather_key)
         data = dict()
         data['time'] = {'value': out_time}
         data['words'] = {'value': words}
@@ -86,4 +84,4 @@ if __name__ == '__main__':
         res = wm.send_template(user_id, template_id, data)
         print(res)
         num += 1
-    print(f"成功发送{num}条信息")
+        print(f"成功发送{num}条信息")
